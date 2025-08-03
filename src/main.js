0bac1bf393ecd0e5584eeeb4aca7f3562751250e
amir <amirtaki623@gmail.com>
2025-08-03 21:38:01 +0330

    js part 3

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 const searchBarContainerEl = document.querySelector(".search-bar-container")
 const manigierEl = document.querySelector(".magnifier");
+
+manigierEl.addEventListener('click', ()=> {
+    searchBarContainerEl.classList.toggle("active")
+})
